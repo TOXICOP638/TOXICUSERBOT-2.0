a89079247131102322b8916b6d227a8b789c78c0
@Devil6694 <153984431+Samyak1222@users.noreply.github.com>
2024-02-28 19:45:14 +0530

    Update start.py

--- Zaid/modules/bot/start.py
+++ Zaid/modules/bot/start.py
@@ -1,5 +1,5 @@
 from Zaid import app, API_ID, API_HASH
-from config import OWNER_ID, ALIVE_PIC
+from config import ALIVE_PIC
 from pyrogram import filters
 import os
 import re
@@ -9,35 +9,38 @@ from pyrogram import *
 from pyrogram.types import * 
 
 PHONE_NUMBER_TEXT = (
-    "✘ Heya My Master👋!\n\n✘ I'm Your Assistant?\n\n‣ I can help you to host Your Left Clients.\n\n‣ String Session Bot: @Devil_superstringbot \n\n‣ This specially for Buzzy People's(lazy)\n\n‣ Now /clone {send your PyroGram String Session}"
+    " ✦𝗛𝗘𝗬..! 𝗠𝗔𝗦𝗧𝗘𝗥..!!👋!\n\n✦ 𝗜'𝗠 𝗔 𝗣𝗢𝗪𝗘𝗥𝗙𝗨𝗟 DEVIL 𝗜𝗗 𝗨𝗦𝗘𝗥𝗕𝗢𝗧 𝗛𝗘𝗟𝗣𝗘𝗥?\n\n‣ 𝗜 𝗖𝗔𝗡 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨 𝗧𝗢 𝗛𝗢𝗦𝗧 𝗬𝗢𝗨𝗥 𝗟𝗘𝗙𝗧 𝗖𝗟𝗜𝗘𝗡𝗧𝗦.\n\n‣ 𝗛𝗘𝗟𝗣𝗘𝗥 ✦: [STRING SESSION BOT](https://t.me/Devil_superstringbot) \n\n‣ 𝗧𝗛𝗜𝗦 𝗜𝗦 𝗦𝗣𝗘𝗖𝗜𝗔𝗟𝗟𝗬 𝗙𝗢𝗥 𝗚𝗔𝗡𝗗𝗨 𝗣𝗘𝗢𝗣𝗟𝗘'𝗦(ʟᴀᴢʏ)\n\n‣ 𝗡𝗢𝗪 /clone {send your PyroGram String Session}"
 )
 
-@app.on_message(filters.user(OWNER_ID) & filters.command("start"))
+@app.on_message(filters.command("start"))
 async def hello(client: app, message):
     buttons = [
            [
-                InlineKeyboardButton("⚡DEVIL⚡", url="t.me/Devil6694"),
+                InlineKeyboardButton("⚡𝗗𝗘𝗣𝗟𝗢𝗣𝗘𝗥⚡", url="t.me/Devil6694"),
             ],
             [
-                InlineKeyboardButton("⚡GROUP⚡", url="t.me/your_devillok"),
+                InlineKeyboardButton("⚡𝗨𝗣𝗗𝗔𝗧𝗘𝗦⚡", url="t.me/your_devil82"),
+            ],
+            [
+                InlineKeyboardButton("⚡𝗦𝗨𝗣𝗣𝗢𝗥𝗧⚡", url="t.me/your_devillok"),
             ],
             ]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_photo(message.chat.id, ALIVE_PIC, caption=PHONE_NUMBER_TEXT, reply_markup=reply_markup)
 
-# © By Itz-Zaid Your motherfucker if uh Don't gives credits.
-@app.on_message(filters.user(OWNER_ID) & filters.command("clone"))
+# © By itzshukla Your motherfucker if uh Don't gives credits.
+@app.on_message(filters.command("clone"))
 async def clone(bot: app, msg: Message):
     chat = msg.chat
     text = await msg.reply("Usage:\n\n /clone session")
     cmd = msg.command
     phone = msg.command[1]
     try:
-        await text.edit("Booting Your Client")
+        await text.edit("RUK JA BETA...💌")
                    # change this Directry according to ur repo
         client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="Zaid/modules"))
         await client.start()
         user = await client.get_me()
-        await msg.reply(f"Your Client Has Been Successfully As {user.first_name} ✅.")
+        await msg.reply(f" 💘𝑨𝑩 𝑻𝑼𝑴 𝑷𝑨𝑲𝑲𝑨 𝑽𝑨𝑳𝑬 DEVIL 𝑩𝑬𝑻𝑬 𝑨𝑺 ❥ {user.first_name} 💨.")
     except Exception as e:
         await msg.reply(f"**ERROR:** `{str(e)}`\nPress /start to Start again.")
