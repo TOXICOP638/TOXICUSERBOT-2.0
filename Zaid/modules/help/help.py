4186e383c0241ad55860ca8eb7b1cd1fffd9f581
TOXICOP638 <149950962+TOXICOP638@users.noreply.github.com>
2024-03-01 09:35:16 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -38,13 +38,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "⚡DEVIL XD OP⚡"
+            ac.title = "😈TEAM CRIMINALS OP😈"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @your_devillok × @your_devil82 •",
+                f"```{str(ac)}```\n• @TCF_OFFICIAL × @NOOBS_ALWAYS_OP •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @your_devil82"
+            this_command += "© @NOOBS_ALWAYS_OP"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @your_devillok × @your_devil82 •"
+            message, f"```{str(ac)}```\n• @TCF_OFFICIAL × @NOOBS_ALWAYS_OP •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -97,7 +97,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @your_devil82"
+            this_command += "© @NOOBS_ALWAYS_OP"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
