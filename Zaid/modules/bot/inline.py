3696c26be8d017ab88d19ccd1dffbf0033254782
𝙏𝙊𝙓𝙄𝘾-𝙓𝘿 <149950962+TOXICOP638@users.noreply.github.com>
2024-06-06 20:14:39 +0530

    Update inline.py

--- Zaid/modules/bot/inline.py
+++ Zaid/modules/bot/inline.py
@@ -50,7 +50,7 @@ async def get_readable_time(seconds: int) -> str:
 async def alive_function(message: Message, answers):
     uptime = await get_readable_time((time.time() - StartTime))
     msg = f"""
-<b> — Hey, I am alive.</b>
+<b> — 𝗟𝗘𝗚𝗘𝗡𝗗𝗦 𝗨𝗦𝗘𝗥𝗕𝗢𝗧 𝗜𝗦 𝗔𝗟𝗪𝗔𝗬𝗦 𝗔𝗟𝗜𝗩𝗘😈.</b>
 
 <b> • User :</b> {message.from_user.mention}
 <b> • Plugins :</b> <code>{len(CMD_HELP)} Modules</code>
